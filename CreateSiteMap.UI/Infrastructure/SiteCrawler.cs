2bdd4e8d5fd6131be1f1f67c0aab063d7120383d
AnAmALiyA <safeshka@gmail.com>
2016-09-18 01:14:20 +0300

    Update SiteCrawler.cs

--- CreateSiteMap.UI/Infrastructure/SiteCrawler.cs
+++ CreateSiteMap.UI/Infrastructure/SiteCrawler.cs
@@ -22,16 +22,31 @@ namespace CreateSiteMap.UI.Infrastructure
 
         private string CorrectUrl(string url)
         {
-            string lastSymbol = url.Substring(url.Length - 1, 1);
-            if (lastSymbol == "/" || lastSymbol == "#") //delete '/' or '#' from the end of url if it exist
-                url = url.Remove(url.Length - 1);
+            string lastSymbol = string.Empty;          
+
+            if (url.Length>1)
+            {
+                    lastSymbol = url.Substring(url.Length - 2, 2); 
+                              
+                if (lastSymbol == "/#")
+                {
+                    url = url.Remove(url.Length - 2);
+                    return url;
+                }
+            }            
+           
+                lastSymbol = url.Substring(url.Length - 1, 1);
+                if (lastSymbol == "/" || lastSymbol == "#")
+                {
+                    url = url.Remove(url.Length - 1);
+                    return url;
+                }
 
             return url;
         }
 
         public async Task<IEnumerable<MesuaredPage>> CrawlAsync(string url)
         {
-            //Представляет потокобезопасную коллекцию пар "ключ-значение", доступ к которой могут одновременно получать несколько потоков.
             ConcurrentDictionary<string, MesuaredPage> crawledPagesDictionary = new ConcurrentDictionary<string, MesuaredPage>();
             string host = new HelperService().GetHostFromUrl(url);
 
@@ -39,8 +54,7 @@ namespace CreateSiteMap.UI.Infrastructure
 
             return crawledPagesDictionary.Select(x => x.Value).ToList();
         }
-
-        //Хитрость этого метода в том, что он создаёт временный List. В него сохраняет найденное и проверяет перед сохранением в базовый Dictionaty
+        
         private async Task CrawlPage(ConcurrentDictionary<string, MesuaredPage> crawledPagesDictionary, string host, string url)
         {
             if (string.IsNullOrWhiteSpace(url))
@@ -51,9 +65,9 @@ namespace CreateSiteMap.UI.Infrastructure
                 return;
             }
 
-            List<string> pagesCrawledList = new List<string>(); //list of parsed links
-            string resultHttpClient = ""; //result of HttpClient
-            Regex regexLink = new Regex(@"<a.*?href=[""'](?<url>.*?)[""'].*?>(?<name>.*?)</a>"); //reg to find links on html page
+            List<string> pagesCrawledList = new List<string>();
+            string resultHttpClient = "";
+            Regex regexLink = new Regex("\\<a\\s*href\\s*=\\s*(?:[\"'](?<url>[^\"']*)[\"']|(?<url>\\S+))");
             MatchCollection matches;
             Stopwatch stopwatch = new Stopwatch();
 
@@ -66,7 +80,6 @@ namespace CreateSiteMap.UI.Infrastructure
                 {
                     stopwatch.Stop();
 
-                    //if http status not 200 - stop executing method Если удачно, то false и я не зайду в if
                     if (!response.IsSuccessStatusCode)
                     {
                         return;
@@ -76,19 +89,18 @@ namespace CreateSiteMap.UI.Infrastructure
                     matches = regexLink.Matches(resultHttpClient);
                 }
             }
-            catch (HttpRequestException) //raises when has been pased nonexistent url -> angular ui url or something else
-            {
-                return; //nothing to do when url not exist
-            }
-
-            bool isAdded = crawledPagesDictionary.TryAdd(url, new MesuaredPage { Url = url, ResponseTime = (int)stopwatch.ElapsedMilliseconds, Date = DateTime.Now });
-
-            if (!isAdded || matches.Count == 0) //if page already crawled or hasn't links
+            catch (HttpRequestException)
             {
                 return;
             }
 
-            //get parsed links
+            bool isAdded = crawledPagesDictionary.TryAdd(url, new MesuaredPage { Url = url, ResponseTime = (int)stopwatch.ElapsedMilliseconds, Date = DateTime.Now });
+
+            if (!isAdded || matches.Count == 0)
+            {
+                return;
+            }
+
             string link = string.Empty;
             foreach (Match match in matches)
             {
@@ -98,12 +110,16 @@ namespace CreateSiteMap.UI.Infrastructure
                     continue;
                 }
 
-                if (!link.StartsWith("http") && !link.StartsWith("www") && !link.StartsWith(host)) //transform absolute path to absolure url
+                if (!link.StartsWith("http") && !link.StartsWith("www") && !link.StartsWith(host))
                 {
-                    link = host + "/" + link;
+                    if (!link.StartsWith("/"))
+                    {
+                        link = "/" + link;
+                    }
+                    link = host + link;
                 }
 
-                if (link.StartsWith(host)) //get only internal links
+                if (link.StartsWith(host))
                 {
                     link = CorrectUrl(link);
 
@@ -114,12 +130,11 @@ namespace CreateSiteMap.UI.Infrastructure
                 }
             }
 
-            if (pagesCrawledList.Count == 0)//if html page hasn't internal links
+            if (pagesCrawledList.Count == 0)
             {
                 return;
             }
 
-            //check MaxPageToCrawling. Тут условия длины проверки.
             int length = pagesCrawledList.Count;
             if (pagesCrawledList.Count + crawledPagesDictionary.Count > MaxPageAllowed)
             {
